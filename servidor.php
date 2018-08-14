4646b269bcfff845829e1f16ef5a1c160dacea56
Bruno Sousa <bruninho_brian@hotmail.com>
2018-08-14 18:02:41 -0300

    mudando titulo e descrição para melhorar a busca

--- servidor.php
+++ servidor.php
@@ -8,17 +8,16 @@ $menu = [
 ];
 
 $dados = [
-  ['title'=>"Notícia 1", 'description'=>"Descrição do cartão 1", 'detail'=>"Texto de detalhe do cartão 1", 'image'=>"http://materializecss.com/images/office.jpg", 'link'=>"#link"],
-  ['title'=>"Cartão 2", 'description'=>"Descrição do cartão 2", 'detail'=>"Texto de detalhe do cartão 2", 'image'=>"http://materializecss.com/images/office.jpg", 'link'=>"#link"],
-  ['title'=>"Cartão 3", 'description'=>"Descrição do cartão 3", 'detail'=>"Texto de detalhe do cartão 3", 'image'=>"http://materializecss.com/images/office.jpg", 'link'=>"#link"],
-  ['title'=>"Cartão 4", 'description'=>"Descrição do cartão 4", 'detail'=>"Texto de detalhe do cartão 4", 'image'=>"http://materializecss.com/images/office.jpg", 'link'=>"#link"],
-  ['title'=>"Cartão 5", 'description'=>"Descrição do cartão 5", 'detail'=>"Texto de detalhe do cartão 5", 'image'=>"http://materializecss.com/images/office.jpg", 'link'=>"#link"],
-  ['title'=>"Cartão 6", 'description'=>"Descrição do cartão 6", 'detail'=>"Texto de detalhe do cartão 6", 'image'=>"http://materializecss.com/images/office.jpg", 'link'=>"#link"],
-  ['title'=>"Cartão 7", 'description'=>"Descrição do cartão 7", 'detail'=>"Texto de detalhe do cartão 7", 'image'=>"http://materializecss.com/images/office.jpg", 'link'=>"#link"],
-  ['title'=>"Cartão 8", 'description'=>"Descrição do cartão 8", 'detail'=>"Texto de detalhe do cartão 8", 'image'=>"http://materializecss.com/images/office.jpg", 'link'=>"#link"],
-  ['title'=>"Cartão 9", 'description'=>"Descrição do cartão 9", 'detail'=>"Texto de detalhe do cartão 9", 'image'=>"http://materializecss.com/images/office.jpg", 'link'=>"#link"],
-  ['title'=>"Cartão 10", 'description'=>"Descrição do cartão 10", 'detail'=>"Texto de detalhe do cartão 10", 'image'=>"http://materializecss.com/images/office.jpg", 'link'=>"#link"],
-  ['title'=>"Cartão 11", 'description'=>"Descrição do cartão 11", 'detail'=>"Texto de detalhe do cartão 11", 'image'=>"http://materializecss.com/images/office.jpg", 'link'=>"#link"]
+  ['title'=>"Futebol", 'description'=>"Futebol 2018", 'detail'=>"Texto de detalhe do cartão 1", 'image'=>"http://materializecss.com/images/office.jpg", 'link'=>"#link"],
+  ['title'=>"Novela", 'description'=>"As melhores novelas", 'detail'=>"Texto de detalhe do cartão 2", 'image'=>"http://materializecss.com/images/office.jpg", 'link'=>"#link"],
+  ['title'=>"Séries", 'description'=>"As séries premiadas", 'detail'=>"Texto de detalhe do cartão 3", 'image'=>"http://materializecss.com/images/office.jpg", 'link'=>"#link"],
+  ['title'=>"Filmes", 'description'=>"Filmes em destaque", 'detail'=>"Texto de detalhe do cartão 4", 'image'=>"http://materializecss.com/images/office.jpg", 'link'=>"#link"],
+  ['title'=>"Desenhos", 'description'=>"Desenhos Animados", 'detail'=>"Texto de detalhe do cartão 5", 'image'=>"http://materializecss.com/images/office.jpg", 'link'=>"#link"],
+  ['title'=>"Programas", 'description'=>"Todos os Programas", 'detail'=>"Texto de detalhe do cartão 6", 'image'=>"http://materializecss.com/images/office.jpg", 'link'=>"#link"],
+  ['title'=>"BBB", 'description'=>"BBB 19", 'detail'=>"Texto de detalhe do cartão 7", 'image'=>"http://materializecss.com/images/office.jpg", 'link'=>"#link"],
+  ['title'=>"A Fazenda", 'description'=>"A fazenda 2018", 'detail'=>"Texto de detalhe do cartão 8", 'image'=>"http://materializecss.com/images/office.jpg", 'link'=>"#link"],
+  ['title'=>"Encrenca", 'description'=>"Encrenca redetv", 'detail'=>"Texto de detalhe do cartão 9", 'image'=>"http://materializecss.com/images/office.jpg", 'link'=>"#link"],
+  ['title'=>"MasterChef", 'description'=>"O melhor da Band", 'detail'=>"Texto de detalhe do cartão 10", 'image'=>"http://materializecss.com/images/office.jpg", 'link'=>"#link"],
 ];
 
 
